c59d8c572f3ac7fc3fe8087ca499915888d3b5d6
superhuman005 <sam.ola13@gmail.com>
2026-02-25 20:39:11 +0100

    update

--- Server/dist/server.js
+++ Server/dist/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
+const connectDB = require("./src/config/db");
 // Connect to MongoDB
 connectDB();
 const app = express();
@@ -15,9 +15,9 @@ app.use(cors({
 app.use(cors());
 app.use(express.json());
 // Routes
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/appointments", require("./routes/appointmentRoutes"));
-app.use("/api/medical", require("./routes/medicalRoutes"));
+app.use("/api/auth", require("./src/routes/authRoutes"));
+app.use("/api/appointments", require("./src/routes/appointmentRoutes"));
+app.use("/api/medical", require("./src/routes/medicalRoutes"));
 // Port
 const PORT = process.env.PORT || 3000;
 // Start server
