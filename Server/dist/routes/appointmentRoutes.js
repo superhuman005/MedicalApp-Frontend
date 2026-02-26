8d04d26f13ba113ce8de1a9c0b0d7e7c93ffd6e5
superhuman005 <sam.ola13@gmail.com>
2026-02-27 00:16:14 +0100

    initial commit

--- Server/dist/routes/appointmentRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const auth = require("../middleware/authMiddleware");
-const { bookAppointment, getAppointments } = require("../controllers/appointmentController");
-router.post("/", auth, bookAppointment);
-router.get("/", auth, getAppointments);
-module.exports = router;
-//# sourceMappingURL=appointmentRoutes.js.map
--- Server/src/validation/authValidation.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-exports.signupSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-  phone: Joi.string().required(),
-  role: Joi.string().valid("doctor", "patient")
-});
